ad3aa34d79b8cdc461188a789097d4e99aa7b82c
Fre Goe <frederic.goetinck.moret@telenet.be>
2024-09-22 13:16:39 +0200

    Fixed theme change to keep theme on different pages

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,50 +1,71 @@
 
 
 // Change theme with button
+let theme;
+theme = localStorage.getItem('myTheme');
+
+function toDark() {
+    const colorsBlue = document.getElementsByClassName("cBlue");
+    const colorsRed = document.getElementsByClassName("cRed");
+    const bgsBlue = document.getElementsByClassName("bgBlue");
+    const bgsYellow = document.getElementsByClassName("bgYellow");
+
+    for (const colorBlue of colorsBlue) {
+        colorBlue.style.color= "#b7c4e1";
+    }
+    for (const colorRed of colorsRed) {
+        colorRed.style.color= "#da7b58";
+    }
+    for (const bgBlue of bgsBlue) {
+        bgBlue.style.backgroundColor= "#48536c";
+    }
+    for (const bgYellow of bgsYellow) {
+        bgYellow.style.backgroundColor= "#645e40";
+    }
+    document.querySelector('html').style.backgroundColor = "#2c2c2c";
+}
+
+function toLight() {
+    let colorsBlue = document.getElementsByClassName('cBlue');
+    let colorsRed = document.getElementsByClassName('cRed');
+    let bgsBlue = document.getElementsByClassName('bgBlue');
+    let bgsYellow = document.getElementsByClassName('bgYellow');
+
+    for (const colorBlue of colorsBlue) {
+        colorBlue.style.color= "";
+    }
+    for (const colorRed of colorsRed) {
+        colorRed.style.color= "";
+    }
+    for (const bgBlue of bgsBlue) {
+        bgBlue.style.backgroundColor= "";
+    }
+    for (const bgYellow of bgsYellow) {
+        bgYellow.style.backgroundColor= "";
+    }
+    document.querySelector('html').style.backgroundColor = "";
+}
+
+function toggleThemeClass() {
+    document.querySelector('button').classList.toggle('darkTheme');
+    document.querySelector('button').classList.toggle('lightTheme');
+}
+
+if (theme === "dark") {
+    toDark();
+    toggleThemeClass();
+}
 
 function changeTheme() {
     if (document.querySelector('button').className.includes("lightTheme")) {
-        const colorsBlue = document.getElementsByClassName("cBlue");
-        const colorsRed = document.getElementsByClassName("cRed");
-        const bgsBlue = document.getElementsByClassName("bgBlue");
-        const bgsYellow = document.getElementsByClassName("bgYellow");
-
-        for (const colorBlue of colorsBlue) {
-            colorBlue.style.color= "#b7c4e1";
-        }
-        for (const colorRed of colorsRed) {
-            colorRed.style.color= "#da7b58";
-        }
-        for (const bgBlue of bgsBlue) {
-            bgBlue.style.backgroundColor= "#48536c";
-        }
-        for (const bgYellow of bgsYellow) {
-            bgYellow.style.backgroundColor= "#645e40";
-        }
-        document.querySelector('html').style.backgroundColor = "#2c2c2c";
+        toDark();
+        theme = "dark";
     } else {
-        let colorsBlue = document.getElementsByClassName('cBlue');
-        let colorsRed = document.getElementsByClassName('cRed');
-        let bgsBlue = document.getElementsByClassName('bgBlue');
-        let bgsYellow = document.getElementsByClassName('bgYellow');
-
-        for (const colorBlue of colorsBlue) {
-            colorBlue.style.color= "";
-        }
-        for (const colorRed of colorsRed) {
-            colorRed.style.color= "";
-        }
-        for (const bgBlue of bgsBlue) {
-            bgBlue.style.backgroundColor= "";
-        }
-        for (const bgYellow of bgsYellow) {
-            bgYellow.style.backgroundColor= "";
-        }
-        document.querySelector('html').style.backgroundColor = "";
-        
+        toLight();
+        theme = "light";
     }
-    document.querySelector('button').classList.toggle('darkTheme');
-    document.querySelector('button').classList.toggle('lightTheme');
+    toggleThemeClass();
+    localStorage.setItem('myTheme', theme);
 };
 
 document.querySelector('button').onclick = changeTheme;
